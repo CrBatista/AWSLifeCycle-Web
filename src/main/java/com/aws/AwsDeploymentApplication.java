4caa636cb706f560771094442acaca434f24a030
CrBatista <cristianbatista@outlook.es>
2020-05-22 18:02:14 +0100

    Added minimal viable product

---
+++ src/main/java/com/aws/AwsDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDeploymentApplication.class, args);
+	}
+
+}
